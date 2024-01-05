8ecf99cd89dc81a9086cd834fb46a8950c815ab7
Lucas <123523359+lsbrel@users.noreply.github.com>
2024-01-05 14:15:52 -0300

    ✨ feat: modelos de tabelas criados

---
+++ app/Models/Refrigeracao/Aparelho.php
@@ -0,0 +1,11 @@
+<?php
+
+namespace App\Models\Refrigeracao;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Aparelho extends Model
+{
+    use HasFactory;
+}
