1005eb5c226a88b87be8598ae5a2e9b407aefd8b
Lucas <123523359+lsbrel@users.noreply.github.com>
2024-01-05 13:55:03 -0300

    ✨ feat: novos controllers

---
+++ app/Http/Controllers/Admin/Relatorio/RelatorioController.php
@@ -0,0 +1,11 @@
+<?php
+
+namespace App\Http\Controllers\Admin\Relatorio;
+
+use App\Http\Controllers\Controller;
+use Illuminate\Http\Request;
+
+class RelatorioController extends Controller
+{
+    //
+}
---
+++ app/Http/Controllers/Refrigeracao/Endereco/EnderecoController.php
@@ -0,0 +1,11 @@
+<?php
+
+namespace App\Http\Controllers\Refrigeracao\Endereco;
+
+use App\Http\Controllers\Controller;
+use Illuminate\Http\Request;
+
+class EnderecoController extends Controller
+{
+    //
+}
