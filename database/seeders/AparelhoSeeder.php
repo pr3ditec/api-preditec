6b31e9ccaaaa464851a99cdeed3f47fcb8cbe583
Lucas <123523359+lsbrel@users.noreply.github.com>
2024-01-16 15:36:23 -0300

    updates

--- database/seeders/AparelhoSeeder.php
+++ database/seeders/AparelhoSeeder.php
@@ -16,6 +16,11 @@ class AparelhoSeeder extends Seeder
             ["nome" => mb_strtoupper("Ar Condicionado"), "modelo" => "", "usuario_id" => "2"],
             ["nome" => mb_strtoupper("Ar Split"), "modelo" => "", "usuario_id" => "2"],
             ["nome" => mb_strtoupper("Ar Portatil"), "modelo" => "", "usuario_id" => "2"],
+            ["nome" => mb_strtoupper("Ar Piso Teto"), "modelo" => "", "usuario_id" => "2"],
+            ["nome" => mb_strtoupper("Ar Dutado"), "modelo" => "", "usuario_id" => "2"],
+            ["nome" => mb_strtoupper("Ar Janela"), "modelo" => "", "usuario_id" => "2"],
+
+            ["nome" => mb_strtoupper("acj"), "modelo" => "", "usuario_id" => "2"],
 
         ];
 
